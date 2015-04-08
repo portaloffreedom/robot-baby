2d6e0ed848aa2135d83297f0884fdd3043152a04
Berend Weel <b.weel@vu.nl>
2015-04-08 15:47:57 +0200

    First project creation

---
+++ RobotController/RobotController/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RobotController
+//
+//  Created by Berend Weel on 08/04/15.
+//  Copyright (c) 2015 Berend Weel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
