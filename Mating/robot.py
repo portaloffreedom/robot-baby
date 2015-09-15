e77ceaccb3c95b3395b255145ddd37cca972bec0
Rukshar <ruky_wh@hotmail.com>
2015-09-15 12:40:57 +0200

    Created Evolutionary Robot TODO agreement to mate

--- Mating/robot.py
+++ Mating/robot.py
@@ -1,20 +1,25 @@
 from client import Client
 from message import Message
+from random import uniform
 from server import Server
-from time import sleep
 
 
 class Robot():
 
     def __init__(self, name=None):
-        self.server = Server(service=self.server_data)
-        self.client = Client(criterion=self.client_criterion,
-                             response=self.client_data)
+        self.server = Server()
+        self.client = Client()
+        self.initialize_parameters()
         self.name = name
         self.server.daemon = True
         self.client.daemon = True
         self.server.start()
         self.client.start()
+
+    def initialize_parameters(self):
+        self.server.service = self.server_data
+        self.client.criterion = self.client_criterion
+        self.client.response = self.client_data
 
     def server_data(self):
         return Message(hash(self), '')
@@ -24,3 +29,29 @@ class Robot():
 
     def client_data(self):
         return Message(hash(self), '')
+
+
+class EvolutionaryRobot(Robot):
+
+    def __init__(self, name, mate_probability=1):
+        Robot.__init__(self, name)
+        self.mate_probability = mate_probability
+
+    def initialize_parameters(self):
+        self.server.service = self.server_data
+        self.client.criterion = self.client_criterion
+        self.client.response = self.client_data
+
+    def server_data(self):
+        return Message(hash(self), 'Mate?')
+
+    def client_criterion(self, data):
+        return data['hash_code'] != hash(self)\
+            and data['message'] == 'Mate?'\
+            and uniform(0, 1) < self.mate_probability
+
+    def client_data(self):
+        return Message(hash(self), 'Sure!')
+
+    def agree_to_mate(self):
+        pass  # TODO
