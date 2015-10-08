bb766b0ae28f74e582f321fe07da2a50dab816b7
Matteo De Carlo <matteo.dek@gmail.com>
2015-10-08 15:41:08 +0200

    small improvements

--- RobotController/learning/rlpower_algorithm.py
+++ RobotController/learning/rlpower_algorithm.py
@@ -2,12 +2,12 @@ import bisect
 import logging
 import numpy as np
 import random
-from rlpower_controller import RLPowerController
+from learning.rlpower_controller import RLPowerController
 
 __author__ = 'matteo'
 
-class RLPowerAlgorithm:
 
+class RLPowerAlgorithm:
     def __init__(self, config_parameters):
         # TODO: get the following parameters from config file
         self.RANKING_SIZE = 10
@@ -18,33 +18,36 @@ class RLPowerAlgorithm:
 
         self.ranking = []
         # Spline initialisation
-        self._current_spline = np.array([[0.5 + random.normalvariate(0, self.SIGMA) for x in range(self._spline_size)] for y in range(self.NUM_SERVOS)])
+        self._current_spline = np.array(
+            [[0.5 + random.normalvariate(0, self.SIGMA) for x in range(self._spline_size)] for y in
+             range(self.NUM_SERVOS)])
         self.controller = RLPowerController(self._current_spline)
 
     def next_evaluation(self, controller):
         current_fitness = self.get_current_fitness()
         self.save_in_ranking(current_fitness, self._current_spline)
         # Add random noise to the spline
-        uniform = np.array([[random.normalvariate(0, self.SIGMA) for x in range(self._spline_size)] for y in range(self.NUM_SERVOS)])
+        uniform = np.array(
+            [[random.normalvariate(0, self.SIGMA) for x in range(self._spline_size)] for y in range(self.NUM_SERVOS)])
         # Add a weighted average of the best splines seen so far
         total = 0
         modifier = np.zeros(self._current_spline.shape)
         for (fitness, spline) in self.ranking:
             total += fitness
             modifier += (spline - self._current_spline) * fitness
-        self._current_spline = self._current_spline + uniform + modifier/total
+        self._current_spline = self._current_spline + uniform + modifier / total
         self.controller.set_spline(self._current_spline)
 
     def get_current_fitness(self):
         # Manual fitness evaluation
         if self._fitness_evaluation == 'manual':
-            fitness = float(raw_input("Enter fitness of current gait: "))
+            fitness = float(input("Enter fitness of current gait: "))
         # Random fitness (for testing purposes)
         elif self._fitness_evaluation == 'random':
             fitness = 5 + random.normalvariate(0, 2)
         # TODO: evaluate fitness automatically
         elif self._fitness_evaluation == 'auto':
-            pass
+            raise NotImplementedError("auto mode fitness evaluation not ready")
         else:
             logging.error("Unknown fitness evaluation method")
             raise NameError("Unknown fitness evaluation method")
