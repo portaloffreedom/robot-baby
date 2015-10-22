b17577a23789f4f5c933e188e94507d649c0a558
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2015-10-22 12:08:02 +0200

    Added support for tests in Mating, also added __init__.py in modules

--- Mating/run.py
+++
@@ -1,9 +0,0 @@
-from robot import EvolutionaryRobot
-
-r1 = EvolutionaryRobot('gecko')
-r2 = EvolutionaryRobot('spider')
-
-while True:
-    command = raw_input()
-    if command == 'exit':
-        break
